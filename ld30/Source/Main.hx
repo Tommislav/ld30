4b8511598c17de0b68da59f7153bc1f76d0e7656
tommislav <tommy.salomonsson@istop.se>
2014-08-25 01:06:23 +0200

    new music

--- ld30/Source/Main.hx
+++ ld30/Source/Main.hx
@@ -53,7 +53,7 @@ class Main extends Engine {
 		
 		
 		//HXP.screen.color = 0xcccccc;
-		HXP.scene = new IronScene();
+		HXP.scene = new StartScenee();
 	}
 	
 	public function changeLevel(name:String) 
